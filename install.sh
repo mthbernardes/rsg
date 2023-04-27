6d6d33a9cffc7d677c80cc26a14d46201a0b5301
Filippo Lauria <filippo.lauria@iit.cnr.it>
2023-04-27 12:15:14 +0200

    Corrected wrong output on symbolic link error

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ fi
 
 if [ -L "$DST" ]; then
     cat <<EOF
-A symbolic link to {$SRC} already exists.
+A symbolic link to ${SRC} already exists.
 In order to proceed, please run as root the following command
     rm -f "$DST"
 then relaunch this installation script.
