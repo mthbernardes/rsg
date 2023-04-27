1a3841ba61d24dbf2bce1a0a457d9d697bfd58a1
Filippo Lauria <filippo.lauria@iit.cnr.it>
2023-04-27 12:12:50 +0200

    Fixed some exit on error issues

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ fi
 
 if [ ! -f "$SRC" ]; then
     echo "Missing main program file ($SRC)."
+    exit
 fi
 
 if [ ! -L "$DST" ]; then
@@ -19,6 +20,7 @@ In order to proceed, please run as root the following command
     rm -f "$DST"
 then relaunch this installation script.
 EOF
+    exit
 fi
 
 ln -s "$(realpath ${SRC})" "$DST"
